4481be86f12787e5fe99e00cb4fde3ca29a9d6c0
duxiuxing <duxiuxing@foxmail.com>
2024-12-22 06:35:56 +0800

    add class DeleteGameTdbOffsetBin

---
+++ python/delete_gametdb_offsets_bin.py
@@ -0,0 +1,19 @@
+# -- coding: UTF-8 --
+
+import os
+
+from console_configs import ConsoleConfigs
+from local_configs import LocalConfigs
+
+
+class DeleteGameTdbOffsetBin:
+    def run(self):
+        # gametdb_offsets.bin 是 WiiFlow 生成的缓存文件，删掉才会重新生成
+        plugin_name = ConsoleConfigs.wiiflow_plugin_name()
+        gametdb_offsets_bin_path = os.path.join(
+            LocalConfigs.export_root_folder_path(),
+            f"wiiflow\\plugins_data\\{plugin_name}\\gametdb_offsets.bin",
+        )
+
+        if os.path.exists(gametdb_offsets_bin_path):
+            os.remove(gametdb_offsets_bin_path)
