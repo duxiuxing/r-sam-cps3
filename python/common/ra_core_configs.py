4e43239a228dd98c12e3388d5c9e2af54b2e77eb
R-Sam-1980 <duxiuxing@gmail.com>
2025-02-23 00:21:59 +0800

    add class RA_CoreConfigs

---
+++ python/common/ra_core_configs.py
@@ -0,0 +1,33 @@
+# -- coding: UTF-8 --
+
+import os
+import xml.etree.ElementTree as ET
+
+from local_configs import LocalConfigs
+
+
+class RA_CoreConfigs:
+    def __init__(self, xml_file_name="ra-default-core.xml"):
+        self._items = {}
+        xml_file_path = os.path.join(
+            LocalConfigs.repository_directory(), f"config\\{xml_file_name}"
+        )
+        if not os.path.exists(xml_file_path):
+            print(f"【错误】无效文件 {xml_file_path}")
+        else:
+            tree = ET.parse(xml_file_path)
+            root = tree.getroot()
+            for key, value in root.attrib.items():
+                self._items[key] = value
+
+    def _get_value(self, key, default_value=None):
+        return self._items.get(key, default_value)
+
+    def core_display_name(self):
+        return self._get_value("core-display-name")
+
+    def core_file_name_wii(self):
+        return self._get_value("core-file-name_wii")
+
+    def core_file_name_win(self):
+        return self._get_value("core-file-name_win")
