742982d44b8a6864d8c1d37c61f08f4e9e337d7b
R-Sam-1980 <duxiuxing@gmail.com>
2025-02-23 00:54:34 +0800

    Refactoring class ConsoleConfigs

--- python/common/console_configs.py
+++ python/common/console_configs.py
@@ -1,43 +1,72 @@
 # -- coding: UTF-8 --
 
 import fnmatch
+import os
+import xml.etree.ElementTree as ET
+
+from local_configs import LocalConfigs
+from ra_core_configs import RA_CoreConfigs
 
 
 class ConsoleConfigs:
+    __instance = None
+
+    def __init__(self):
+        if ConsoleConfigs.__instance is not None:
+            raise Exception("请使用类的 _instance() 获取实例")
+        else:
+            ConsoleConfigs.__instance = self
+
+        xml_file_path = os.path.join(
+            LocalConfigs.repository_directory(), "config\\console.xml"
+        )
+        if not os.path.exists(xml_file_path):
+            print(f"【错误】无效文件 {xml_file_path}")
+        else:
+            tree = ET.parse(xml_file_path)
+            root = tree.getroot()
+            self._ra_core_configs = RA_CoreConfigs(root.attrib["ra_core_configs"])
+            self._rom_extension = root.attrib["rom_extension"]
+            self._wiiflow_plugin_name = root.attrib["wiiflow_plugin_name"]
+
     @staticmethod
-    def short_name():
-        # 机种简称
-        return "CPS3"
+    def _instance():
+        # 获取单例实例
+        if ConsoleConfigs.__instance is None:
+            ConsoleConfigs()
+        return ConsoleConfigs.__instance
 
     @staticmethod
     def rom_extension():
-        return ".zip"
+        # ROM 文件的扩展名
+        return ConsoleConfigs._instance()._rom_extension
 
     @staticmethod
     def rom_extension_match(file_name):
-        return fnmatch.fnmatch(file_name, "*.zip")
+        pat = f"*{ConsoleConfigs.rom_extension()}"
+        return fnmatch.fnmatch(file_name, pat)
 
     @staticmethod
-    def ra_default_core_name():
-        # 默认的 RetroArch 核心名称
-        return "Arcade (FB Alpha 2012 CPS-3)"
+    def current_ra_core_configs():
+        # RetroArch 核心信息
+        return ConsoleConfigs._instance()._ra_core_configs
 
     @staticmethod
-    def ra_default_core_file_title():
-        # 默认的 RetroArch 核心文件标题
-        return "fbalpha2012_cps3_libretro"
-
-    @staticmethod
-    def ra_default_playlist_name_en():
-        # 默认的英文游戏列表名称
-        return "R-Sam - CPS3 Arcade"
-
-    @staticmethod
-    def ra_default_playlist_name_zhcn():
-        # 默认的中文游戏列表名称
-        return "认真玩 - CPS3 街机"
+    def set_ra_core_configs(ra_core_configs):
+        ConsoleConfigs._instance()._ra_core_configs = ra_core_configs
 
     @staticmethod
     def wiiflow_plugin_name():
         # WiiFlow 的插件名称
-        return "CPS3"
+        return ConsoleConfigs._instance()._wiiflow_plugin_name
+
+
+if __name__ == "__main__":
+    print(ConsoleConfigs.rom_extension())
+    file_name = "test.zip"
+    if ConsoleConfigs.rom_extension_match(file_name):
+        print(f"{file_name} ROM 文件名匹配成功")
+    else:
+        print(f"{file_name} ROM 文件名不匹配")
+    print(ConsoleConfigs.current_ra_core_configs().core_display_name())
+    print(ConsoleConfigs.wiiflow_plugin_name())
