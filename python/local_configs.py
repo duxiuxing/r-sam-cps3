dc35e3c3233f27c8ca7e644ab7052d9ba93121ab
duxiuxing <duxiuxing@foxmail.com>
2026-02-21 21:21:50 +0800

    code review : class LocalConfigs

--- python/local_configs.py
+++ python/local_configs.py
@@ -1,58 +1,44 @@
 # -- coding: UTF-8 --
 
 import os
-import xml.etree.ElementTree as ET
-
 from pathlib import Path
 
 
 class LocalConfigs:
-    __instance = None
-
-    def __init__(self):
-        if LocalConfigs.__instance is not None:
-            raise Exception("请使用 LocalConfigs._instance() 获取实例")
-        else:
-            LocalConfigs.__instance = self
-
-        self._repository_directory = Path(os.getcwd())
-
-        xml_file_path = self._repository_directory.joinpath("config\\local.xml")
-        if not xml_file_path.exists():
-            print(f"【错误】无效文件 {xml_file_path}")
-        else:
-            tree = ET.parse(xml_file_path)
-            root = tree.getroot()
-            self._seven_zip_exe_path = Path(root.attrib["seven_zip_exe_path"])
-            index = root.attrib["root_directory_export_to_index"]
-            self._root_directory_export_to = Path(
-                root.attrib[f"root_directory_export_to_{index}"]
-            )
-
-    @staticmethod
-    def _instance():
-        # 获取单例实例
-        if LocalConfigs.__instance is None:
-            LocalConfigs()
-        return LocalConfigs.__instance
+    _repository_directory = Path(os.getcwd())
 
     @staticmethod
     def repository_directory():
         # 本地仓库路径
-        return LocalConfigs._instance()._repository_directory
+        return LocalConfigs._repository_directory
+
+    _export_to_directory = None
+
+    @staticmethod
+    def export_to_directory():
+        # 导出根目录路径
+        if LocalConfigs._export_to_directory is None:
+            dir0 = (
+                "C:\\Users\\duxiu\\AppData\\Roaming\\Dolphin Emulator\\Load\\WiiSDSync"
+            )
+            dir1 = "D:\\workspace\\github\\R-Sam-1980\\cps3-temp"
+            dir2 = "X:\\"
+            LocalConfigs._export_to_directory = Path(dir1)
+
+        return LocalConfigs._export_to_directory
+
+    @staticmethod
+    def retroarch_directory():
+        return Path("X:\\RetroArch-Win64")
 
     @staticmethod
     def seven_zip_exe_path():
         # 本机 7z.exe 的路径
-        return LocalConfigs._instance()._seven_zip_exe_path
-
-    @staticmethod
-    def root_directory_export_to():
-        # 导出根目录路径
-        return LocalConfigs._instance()._root_directory_export_to
+        return Path("C:\Program Files\7-Zip\7z.exe")
 
 
 if __name__ == "__main__":
     print(LocalConfigs.repository_directory())
+    print(LocalConfigs.export_to_directory())
+    print(LocalConfigs.retroarch_directory())
     print(LocalConfigs.seven_zip_exe_path())
-    print(LocalConfigs.root_directory_export_to())
