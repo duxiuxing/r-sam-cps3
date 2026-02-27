9afcf3526b5754c3895d88d326a9a772d7ded556
duxiuxing <duxiuxing@foxmail.com>
2026-02-28 02:26:00 +0800

    delete python

--- python/game.py
+++
@@ -1,9 +0,0 @@
-# -- coding: UTF-8 --
-
-
-class Game:
-    def __init__(self, id, en_title, zhcn_title):
-        self.id = id
-        self.en_title = en_title
-        self.zhcn_title = zhcn_title
-        self.rom_list = []
--- python/wiiflow_configs.py
+++
@@ -1,9 +0,0 @@
-# -- coding: UTF-8 --
-
-
-class WiiFlow_Configs:
-    _plugin_name = None
-
-    @staticmethod
-    def plugin_name() -> str:
-        return WiiFlow_Configs._plugin_name
